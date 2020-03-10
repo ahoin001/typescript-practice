7bf65b7ce50e16f1426f5cfe6f2f263a64323b5c
Alexander Hoinville <ahoin001@gmail.com>
2020-03-09 23:45:22 -0400

    Parse numbers for result

--- regular-js.js
+++ regular-js.js
@@ -2,7 +2,7 @@ const button = document.querySelector("button");
 const input1 = document.getElementById("num1");
 const input2 = document.getElementById("num2");
 
-const add = (num1, num2) => num1 + num2;
+const add = (num1, num2) => Number(num1) + Number(num2);
 
 button.addEventListener("click", () => {
     console.log(add(input1.value, input2.value));
