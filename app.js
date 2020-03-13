08a07629bcfd58f1707c7bc29f70f5df6a600dce
Alexander Hoinville <ahoin001@gmail.com>
2020-03-12 23:56:08 -0400

    Type inference in typescript objects

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
-/*
-    Core Data Types
-*/
-// * NOTE: Make parameters expect number type
-var add = function (num1, num2, showResult, phrase) {
-    var result = num1 + num2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return num1 + num2;
-    }
+// * NOTE TYPESCRIPT objects are key->type pairs, not key -> value pairs
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+var person = {
+    name: 'Alexander',
+    age: 24
 };
-var num1 = 5;
-var num2 = 0.9;
-var printResult = true;
-var phrase = "Result is: ";
-add(num1, num2, printResult, phrase);
+console.log(person.name);
--- app.ts
+++ app.ts
@@ -1,28 +1,13 @@
-/* 
-    Core Data Types
-*/
 
-// * NOTE: Make parameters expect number type
-const add = (num1: number, num2: number, showResult: boolean, phrase: string) => {
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
 
-    const result = num1 + num2;
-
-    if (showResult) {
-        console.log(phrase + result);
-    } else {
-
-        return num1 + num2
-
-    }
-
+// * NOTE Hover over person, objects are key->type pairs, not key -> value pairs in typescript, it will not be reassignable to another value
+const person = {
+    name: 'Alexander',
+    age: 24
 }
 
-// * NOTE: Type inference: num1 is initialized as number type, so typescript will have errpr if ever assigned another type to this var 
-const num1 = 5;
-const num2 = 0.9;
-const printResult = true;
-const phrase = "Result is: ";
-
-
-add(num1, num2, printResult, phrase);
-
+console.log(person.name)
