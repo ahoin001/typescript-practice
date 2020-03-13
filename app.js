deb6781bb01a6c2f80d6c24cedd3e992487b6c95
Alexander Hoinville <ahoin001@gmail.com>
2020-03-13 00:07:02 -0400

    Understand Array type inference and using properties from types knowing typescript will alert us if we are expecting a type for that property

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-// * NOTE TYPESCRIPT objects are key->type pairs, not key -> value pairs
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
+// * NOTE Hover over types and properties to see type inference
+// * NOTE Hover over person, objects are key->type pairs, not key -> value pairs in typescript, it will not be reassignable to another value
 var person = {
     name: 'Alexander',
-    age: 24
+    age: 24,
+    hobbies: ['Coding', 'Basketball']
 };
+var favoriteActivities;
+favoriteActivities = ['Sports', 'Eating'];
+// favoriteActivities = 'Sports'    // Will have error because array of strings will be expected
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,13 +1,21 @@
-
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
+// * NOTE Hover over types and properties to see type inference
 
 // * NOTE Hover over person, objects are key->type pairs, not key -> value pairs in typescript, it will not be reassignable to another value
 const person = {
     name: 'Alexander',
-    age: 24
+    age: 24,
+    hobbies: ['Coding', 'Basketball']
 }
 
+let favoriteActivities : string[];
+favoriteActivities = ['Sports','Eating']
+// favoriteActivities = 'Sports'    // Will have error because array of strings will be expected
+
 console.log(person.name)
+
+for ( const hobby of person.hobbies) {
+
+    // Safe to use toUpperCase method from screen because using typescript inference it knows that hobby will be a string from array of strings
+    console.log(hobby.toUpperCase())
+
+}
