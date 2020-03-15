e045be9fdded723d5fbf0c4775e7b79e34d32fdb
Alexander Hoinville <ahoin001@gmail.com>
2020-03-14 23:02:03 -0400

    Use Literal type with union type for more type safety (With compiled app.ts)

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 // * Union Type: A variable can be more than 1 type
-var combine = function (input1, input2) {
+var combine = function (input1, input2, 
+// * Literal Type: Will expect one of these values of this type (Also a union type)
+resultType) {
     var result;
     // * This runtime check is used because we ave different logic for different types
     if (typeof input1 === 'number' && typeof input2 === 'number') {
+        // '+' converts to string
         result = input1 + input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
+    if (resultType === 'as-number') {
+        return +result;
+    }
+    else {
+    }
     return result;
 };
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
-var combinedNames = combine('Alex', 'Codes');
+var combinedNames = combine('Alex', 'Codes', 'as-text');
 console.log(combinedNames);
-var combinedThings = combine('Alex', 50);
+var combinedThings = combine('Alex', 50, 'as-number');
 console.log(combinedThings);
