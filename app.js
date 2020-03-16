f323dce21c419376621fa97d6010956612e5e6a5
Alexander Hoinville <ahoin001@gmail.com>
2020-03-15 21:03:20 -0400

    Use Void types for functions expected to not return anything

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
-// * Union Type: A variable can be more than 1 type
-var combine = function (input1, // Will benumber | string,
-input2, 
-// * Literal Type: Will expect one of these values and of this type (Also a union type)
-resultType // 'as-number' | 'as-text'
-) {
-    var result;
-    // * This runtime check is used because we ave different logic for different types
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        // '+' converts to string
-        result = input1 + input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    if (resultType === 'as-number') {
-        return +result;
-    }
-    else {
-    }
-    return result;
+// * Hover over function to see expected return type
+var add = function (n1, n2) {
+    return n1 + n2;
 };
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combinedNames = combine('Alex', 'Codes', 'as-text');
-console.log(combinedNames);
-var combinedThings = combine('Alex', 50, 'as-number');
-console.log(combinedThings);
+// * Void type means function does no return anything
+var printResult = function (num) {
+    console.log("Result: " + num);
+};
+console.log(printResult(add(5, 12)));
+// printResult(add(5, 12))
--- app.ts
+++ app.ts
@@ -1,46 +1,13 @@
-// * Union Type: A variable can be more than 1 type
-
-//  * Custom Type : Type script will expect these types or literal types
-type Combinable = number | string;
-type ConversionDescription = 'as-number' | 'as-text'
-
-const combine = (
-
-    input1: Combinable, // Will benumber | string,
-    input2: Combinable,
-    // * Literal Type: Will expect one of these values and of this type (Also a union type)
-    resultType: ConversionDescription // 'as-number' | 'as-text'
-
-) => {
-
-    let result;
-
-    // * This runtime check is used because we ave different logic for different types
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-
-        // '+' converts to string
-        result = input1 + input2
-
-    } else {
-        result = input1.toString() + input2.toString()
-    }
-
-
-    if (resultType === 'as-number') {
-        return +result
-    } else {
-
-    }
-
-    return result;
-
+// * Hover over function to see expected return type
+const add = (n1: number, n2: number) => {
+    return n1 + n2
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges)
+// * Void type means function does no return anything (Use instead of undefined)
+// * Void tells TS we expect to not return anything, undefined is if returning an undefined value whch is a rare case
+const printResult = (num: number) => {
+    console.log(`Result: ` + num)
+}
 
-const combinedNames = combine('Alex', 'Codes', 'as-text');
-console.log(combinedNames)
-
-const combinedThings = combine('Alex', 50, 'as-number');
-console.log(combinedThings)
+console.log(printResult(add(5, 12)))
+// printResult(add(5, 12))
