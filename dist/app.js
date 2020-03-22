2fa05163d0287c4021913ccafa3358116fc4d711
Alexander Hoinville <ahoin001@gmail.com>
2020-03-22 14:30:40 -0400

    kGeneric Classes , keyof object, use interface for controlled generic values,Generic constraints for more controlled functions

--- dist/app.js
+++ dist/app.js
@@ -25,3 +25,45 @@ const genericConstraintMerge = (objA, objB) => {
 console.log(genericConstraintMerge({ name: 'Alex' }, { age: 24, hobby: 'Coding' }));
 const mergedGenericConstrinedObj = genericConstraintMerge({ name: 'Alex' }, { age: 24, hobby: 'Coding' });
 mergedGenericConstrinedObj.age;
+const countAndPrint = (element) => {
+    let descriptionText = 'Got no value';
+    if (element.length === 1) {
+        descriptionText = `Got 1 element`;
+    }
+    else if (element.length > 1) {
+        descriptionText = `Got ${element.length} elements`;
+    }
+    return [element, descriptionText];
+};
+console.log(countAndPrint('H'));
+const extractAndConvert = (obj, key) => {
+    return `Value: ${obj[key]}`;
+};
+console.log(extractAndConvert({ name: 'Alex' }, name));
+class DataStorage {
+    constructor() {
+        this.data = [];
+        this.addItem = (item) => {
+            this.data.push(item);
+        };
+        this.removeItem = (item) => {
+            if (this.data.indexOf(item) === -1) {
+                return;
+            }
+            this.data.splice(this.data.indexOf(item), 1);
+        };
+        this.getItems = () => {
+            return [...this.data];
+        };
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem('Alex');
+textStorage.addItem('Bill');
+textStorage.removeItem('Bill');
+console.log(textStorage.getItems());
+const numberStorage = new DataStorage();
+numberStorage.addItem(24);
+numberStorage.addItem(3);
+numberStorage.removeItem(3);
+console.log(numberStorage.getItems());
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,11 @@
 // https://www.typescriptlang.org/docs/handbook/generics.html
+
+/****************************************************************
+        Generic Classes, Generic FUnctions, keyof object constraint,
+                Class Constraints
+
+ *****************************************************************/
+
 /****************************************************************
                           Generics
  A type that is connected to other types. Example, Arryay is a type, but can be an array of strings type
@@ -80,7 +87,7 @@ mergedGenericObj.age;
 
 // * T & U are type vairables, and can expect anything
 // * However this means it can be a string number or object 
-// * With extends, we can constrain our variable to a certain type or union type
+// * With extends, we can constrain our variable to a certain type or union type 
 
 // ? Extending objects will let TS know these parameters should at least be objects
 const genericConstraintMerge = <T extends object, U extends object>(objA: T, objB: U) => {
@@ -97,3 +104,108 @@ console.log(genericConstraintMerge({ name: 'Alex' }, { age: 24, hobby: 'Coding' 
 
 const mergedGenericConstrinedObj = genericConstraintMerge({ name: 'Alex' }, { age: 24, hobby: 'Coding' })
 mergedGenericConstrinedObj.age;
+
+
+/****************************************************************
+                          another Generic function
+ A flexible function that lets TS expect certain methods to be available
+ *****************************************************************/
+
+//  ? We can extend this interface, so our cusom type will be expected to have a length property
+interface Lengthy {
+
+    // * length property that will return a number
+    length: number;
+
+}
+
+// * Explicitly return a tuple of [T, string] for the example
+// * Allows more flexibility than explicitly saying to expect string | array | othertypes
+const countAndPrint = <T extends Lengthy>(element: T): [T, string] => {
+
+    let descriptionText = 'Got no value';
+
+    // ? .length has error without interface because it won't know what type T will be (Will say length does not exsist on type T)
+    if (element.length === 1) {
+
+        descriptionText = `Got 1 element`
+
+
+    } else if (element.length > 1) {
+
+        descriptionText = `Got ${element.length} elements`
+
+    }
+
+    return [element, descriptionText]
+
+}
+
+// ? Will be able to expect String or Array because our T will be expected to have length property
+console.log(countAndPrint('H'))
+
+
+/****************************************************************
+                KeyOf Constraint
+
+ *****************************************************************/
+
+//  * extends Keyof to tell TS to expect this to be key in provided object type
+const extractAndConvert = <T extends object, U extends keyof T>(obj: T, key: U) => {
+
+    return `Value: ${obj[key]}`
+
+}
+
+// * Useful to prevent accessing property in an object that
+console.log(extractAndConvert({ name: 'Alex' }, name))
+
+/****************************************************************
+                Generic Classes
+                Class Constraints
+
+ *****************************************************************/
+
+//  * We use T to be able to push any kind of data type
+class DataStorage<T extends string| number | boolean >{
+
+    // * Error because we haven't specified what data is being stored
+    // private data = []
+
+    // ? TS will allow 
+    private data: T[] = []
+
+    addItem = (item: T) => {
+        this.data.push(item)
+    }
+
+    removeItem = (item: T) => {
+
+        // * index of returns -1 if nothing found
+        if (this.data.indexOf(item) === -1) {
+            return
+        }
+        this.data.splice(this.data.indexOf(item), 1)
+    }
+
+    getItems = () => {
+        return [...this.data]
+    }
+
+
+}
+
+// ? new Class with string type expected in T for class blueprint (above) So TS will know what to expect
+const textStorage = new DataStorage<string>()
+textStorage.addItem('Alex')
+textStorage.addItem('Bill')
+textStorage.removeItem('Bill')
+console.log(textStorage.getItems())
+
+// * NOTE Class logic does not work well for objects, this is just example that object 
+const numberStorage = new DataStorage<number>()
+numberStorage.addItem(24)
+numberStorage.addItem(3)
+// numberStorage.addItem({'Will be error since number is expected not string'})
+numberStorage.removeItem(3)
+console.log(numberStorage.getItems())
